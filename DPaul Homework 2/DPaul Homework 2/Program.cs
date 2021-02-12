500e017c04cb3a7c50eb3589e13ad7750f5948d7
DPaul92 <doctorpaul92@gmail.com>
2021-02-11 22:24:42 -0600

    Homework 2

---
+++ DPaul Homework 2/DPaul Homework 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DPaul_Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring an array of ints
+            int[] numbers;
+            numbers = new int [] { 1, 2, 3, 4, 5, 11, 12, 13, 14, 15 };
+            foreach (int i in numbers)
+            {
+                //using mod 2 if it returns 0 the number is even
+                if (i % 2 == 0)
+                    Console.WriteLine("{0} is even", i);
+                //else it is odd
+                else 
+                    Console.WriteLine("{0} is odd", i);
+            }
+        }
+    }
+}
